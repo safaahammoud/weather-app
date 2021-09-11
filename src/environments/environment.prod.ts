910211cc67b01d85c5e9a871b20527ff1f5266a3
hammoudsafaa@gmail.com <hammoudsafaa@gmail.com>
2021-09-12 01:57:12 +0300

    chore: add environment files

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const productionEnvironment = {
+  production: true,
+  appApiUrl: 'http://api.openweathermap.org/data/2.5/',
+  appApiKey: '993eef70ad22f3e2a63a59153360bcf9',// Currently development environment
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+import { Environment } from './../app/shared/models/environment.model';
+
+export const environment: Environment = {
+  production: false,
+  appApiUrl: 'http://api.openweathermap.org/data/2.5/',
+  appApiKey: '993eef70ad22f3e2a63a59153360bcf9', // For development environment
+};
