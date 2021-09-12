76689f0bc796fce641a8457f1ee0b439dfdd5318
hammoudsafaa@gmail.com <hammoudsafaa@gmail.com>
2021-09-12 13:13:35 +0300

    fix: code style in weather routing file

--- src/app/modules/weather/weather-routing.module.ts
+++ src/app/modules/weather/weather-routing.module.ts
@@ -13,7 +13,10 @@ const routes: Routes = [
         loadChildren: () => import('../weather/components/cities-weather-list/cities-weather-list.module')
             .then(m => m.CitiesWeatherListModule)
       },
-      { path: 'weather-forecast', loadChildren: () => import('./components/city-weather-forecast/city-weather-forecast.module').then(m => m.CityWeatherForecastModule) }
+      {
+        path: 'weather-forecast',
+        loadChildren: () => import('./components/city-weather-forecast/city-weather-forecast.module').then(m => m.CityWeatherForecastModule)
+      }
     ],
   },
 ];
