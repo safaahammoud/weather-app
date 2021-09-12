c051dc917508fc17df16e864fe060d4dc218cfe5
hammoudsafaa@gmail.com <hammoudsafaa@gmail.com>
2021-09-12 17:17:51 +0300

    chore: remove empty tests

--- src/app/shared/pipes/date-util.pipe.spec.ts
+++
@@ -1,7 +0,0 @@
-import { DateUtilPipe } from './date-util.pipe';
-
-describe('DateUtilPipe', () => {
-  it('should be created', () => {
-    expect(DateUtilPipe).toBeTruthy();
-  });
-});
--- src/app/shared/services/alert.service.spec.ts
+++
@@ -1,6 +0,0 @@
-import { AlertService } from './alert.service';
-describe('AlertService', () => {
-  it('should be created', () => {
-    expect(AlertService).toBeTruthy();
-  });
-});
