963e4713dc57d1f16d5a39222091059c15034715
hammoudsafaa@gmail.com <hammoudsafaa@gmail.com>
2021-09-12 05:07:46 +0300

    fix: url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
-export const productionEnvironment = {
+import { Environment } from './../app/shared/models/environment.model';
+
+export const environment: Environment = {
   production: true,
-  appApiUrl: 'http://api.openweathermap.org/data/2.5/',
+  appApiUrl: 'https://api.openweathermap.org/data/2.5/',
   appApiKey: '993eef70ad22f3e2a63a59153360bcf9',// Currently development environment
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ import { Environment } from './../app/shared/models/environment.model';
 
 export const environment: Environment = {
   production: false,
-  appApiUrl: 'http://api.openweathermap.org/data/2.5/',
+  appApiUrl: 'https://api.openweathermap.org/data/2.5/',
   appApiKey: '993eef70ad22f3e2a63a59153360bcf9', // For development environment
 };
