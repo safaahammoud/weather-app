dfed9fce1d3a9b8ae173654831f9339f5ca2e0ce
hammoudsafaa@gmail.com <hammoudsafaa@gmail.com>
2022-07-07 07:26:43 +0300

    try any

--- src/app/modules/weather/components/cities-weather-list/cities-weather-list.module.ts
+++ src/app/modules/weather/components/cities-weather-list/cities-weather-list.module.ts
@@ -7,9 +7,7 @@ import { MaterialModule } from './../../../../shared/modules/material.module';
 import { ComponentModule } from './../../../../shared/components/component.module';
 
 @NgModule({
-  declarations: [
-    CitiesWeatherListComponent,
-  ],
+  declarations: [CitiesWeatherListComponent],
   imports: [
     CommonModule,
     MaterialModule,
